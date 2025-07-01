b0bf5408134c1372df7bc4817ee009b052011e41
KatarinaStjepanovic <katarina22april@gmail.com>
2025-07-01 22:20:24 +0200

    Adding app version

--- calculator/cal.js
+++ calculator/cal.js
@@ -261,6 +261,38 @@ const removeSidebar = () => {
    window.location.href = "../recent/recent.html";
  })
 
+ const btnBackSp = document.getElementById('backspace');
+
+
+ btnBackSp.addEventListener('click', () => {
+    deleteLast();
+ })
+
+ let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault(); 
+  deferredPrompt = e;
+
+  const installBtn = document.getElementById('installBtn');
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted install');
+      } else {
+        console.log('User dismissed install');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+
+
 
 
  
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,33 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) =>
-      cache.addAll(['/', '/index.html', '/style.css', '/script.js'])
-    )
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/style.css',
+        '/colors.css',
+        '/newExp.js',
+        '/service-worker.js',
+
+        '/calculator/cal.css',
+        '/calculator/cal.js',
+
+        '/recent/rec.js',
+        '/recent/rec.css',
+
+        '/favicon.ico',
+        '/favicon-96x96.png',
+        '/favicon.svg',
+        '/apple-touch-icon.png',
+
+        '/web-app-manifest-192x192.png',
+        '/web-app-manifest-512x512.png',
+        '/site.webmanifest',
+
+        '/turnDevice.png'
+      ]);
+    }).catch((err) => {
+      console.error('Greška pri keširanju:', err);
+    })
   );
 });
 
